dbe7d3318c7115a3d398c1474fea80ceea92d11d
WIN_NT\x16g030 <x16g030@chiba-fjb.ac.jp>
2017-12-08 09:53:25 +0900

    追加

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,7 @@
 
 public class Test {public Test() {
-	System.out.println("konnnitiha");
+	System.out.println("こんにちは");
+	System.out.println("こんばんは");
 }
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
